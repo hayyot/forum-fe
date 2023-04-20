7f66725e6d303d911c22ab428654821089dd064c
Hyhayhy <2745553068@qq.com>
2023-04-21 02:05:51 +0800

    feat(*):完善学习界面

--- src/views/Study/StudyIndex.vue
+++ src/views/Study/StudyIndex.vue
@@ -6,42 +6,94 @@
 -->
 <template>
     <div class="StudyIndex-bg">
-      <el-table
-          :data="list"
-          style="width: 100%"
-          row-key="sid"
-          border
-          lazy
-          :load="getChildrens"
-          :tree-props="{children: 'children', hasChildren: 'hasChildren'}"
-      >
-        <el-table-column
-            label=""
-            width="230"
-            align="left"
-        >
-          <template slot-scope="scope">
-            <span>{{ scope.row.sname }}</span>
-          </template>
-        </el-table-column>
-      </el-table>
+<!--      <el-table-->
+<!--          :data="list"-->
+<!--          style="width: 100%"-->
+<!--          row-key="sid"-->
+<!--          border-->
+<!--          lazy-->
+<!--          :load="getChildrens"-->
+<!--          :tree-props="{children: 'children', hasChildren: 'hasChildren'}"-->
+<!--      >-->
+<!--        <el-table-column-->
+<!--            label=""-->
+<!--            width="230"-->
+<!--            align="left"-->
+<!--        >-->
+<!--          <template slot-scope="scope">-->
+<!--            <span>{{ scope.row.sname }}</span>-->
+<!--          </template>-->
+<!--        </el-table-column>-->
+<!--      </el-table>-->
+        <el-row class="tac">
+            <el-col :span="3">
+                <el-menu
+                        default-active="2"
+                        class="el-menu-vertical-demo"
+                        background-color="#fff"
+                        text-color="#000000"
+                        @open="getChildrens"
+                        active-text-color="#66CC99">
+                    <el-submenu v-for="item in list" :key="item.sid" :index="item.sid+''">
+                        <template slot="title">
+                            <span>{{item.sname}}</span>
+                        </template>
+                        <template v-for="item in childrenList">
+                            <el-menu-item :key="item.sid" :index="item.sid+''" @click="getMessage(item.value)">
+                                <span slot="title">{{ item.sname }}</span>
+                            </el-menu-item>
+                        </template>
+                    </el-submenu>
+<!--                    <template v-for="route of list">-->
+<!--                        <el-submenu :key="route.sid" :index="route.sid">-->
+<!--                            <template slot="title" >-->
+<!--                                <span>{{ route.sname }}</span>-->
+<!--                            </template>-->
+<!--                            <template v-for="item in childrenList">-->
+<!--                                <el-menu-item  :key="item.sid" :index="item.sid">-->
+<!--                                    <span slot="title">{{ item.sname }}</span>-->
+<!--                                </el-menu-item>-->
+<!--                            </template>-->
+<!--                        </el-submenu>-->
+<!--                        <template >-->
+<!--                            <el-menu-item :index="route.path" :key="route.path">-->
+<!--                                <span slot="title">{{ route.sname }}</span>-->
+<!--                            </el-menu-item>-->
+<!--                        </template>-->
+<!--                    </template>-->
+
+                </el-menu>
+
+            </el-col>
+<!--            <div v-html="message">-->
+
+<!--            </div>-->
+            <div class="markdown-body" v-html="message"></div>
+        </el-row>
+
     </div>
 </template>
 
 <script>
-import { getStudyList } from '@/api/api';
+import {getStudyList} from '@/api/api';
+// import Prismjs from 'prismjs';
+import axios from "axios";
+import 'github-markdown-css'
 
 export default {
     name: 'ForumFeStudyIndex',
 
     data() {
         return {
-          list: [],
+            list: [],
+            childrenList:[],
+            message:''
         };
     },
 
     mounted() {
       this.getList(0);
+      // Prismjs.highlightAll()
     },
 
     methods: {
@@ -49,15 +101,41 @@ export default {
       getList(id){
         getStudyList(id).then(res =>{
           this.list = res.data;
-          console.log(res.data)
+          // console.log(res.data)
         })
       },
       //判断此目录下是否有子节点
-      getChildrens(tree,treeNode,resolve){
-        getStudyList(tree.sid).then(res => {
-          resolve(res.data);
-          console.log(res.data);
+      getChildrens(id){
+          // console.log("12321")
+        this.childrenList=[];
+        getStudyList(id).then(res => {
+          this.childrenList = res.data;
+          // console.log(res.data);
         })
+      },
+      getMessage(value){
+          let _this=this;
+          // getValue(value).then(res=>{
+          //   this.message=res.data.sneirong
+          //     console.log(res.data.sneirong)
+          //     Prismjs.highlightAll()
+          // })
+          var config = {
+              method: 'get',
+              url: `http://47.107.225.176:8808/getValue/${value}`,
+              headers: {
+                  'User-Agent': 'Apifox/1.0.0 (https://www.apifox.cn)'
+              }
+          };
+
+          axios(config)
+              .then(function (res) {
+                  _this.message=res.data.data.sneirong;
+                  console.log(res.data.data.sneirong)
+              })
+              .catch(function (error) {
+                  console.log(error);
+              });
       }
     },
 };
@@ -68,6 +146,8 @@ export default {
     position: absolute;
     width: 100%;
     height: 100%;
-    background: #f1f1f1;
+}
+.tac{
+  text-align: left;
 }
 </style>
