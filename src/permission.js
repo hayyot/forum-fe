b6f15f105e09523951c167557a925249d278ee5c
Hyhayhy <2745553068@qq.com>
2023-04-29 15:44:20 +0800

    feat(*):完善关于页面

--- src/permission.js
+++ src/permission.js
@@ -6,7 +6,7 @@ router.beforeEach((to, from, next) => {
   if (to.matched.some(record => record.meta.requireAuth)) { // 判断该路由是否需要登录权限
 
     const token = localStorage.getItem("token")
-    // console.log("------------" + token)
+    console.log("------------" + token)
 
     if (token) { // 判断当前的token是否存在 ； 登录存入的token
       if (to.path === 'http://47.107.225.176:8808/login') {
